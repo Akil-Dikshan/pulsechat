d29280c29b0fe8895fff8375bc864e7aefca31bc
Akil-Dikshan <akildikshan02@gmail.com>
2026-03-07 00:21:36 +0530

    chore(PC-1): initialise org repo with client and server directories

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <h1 className="text-3xl font-bold p-4">PulseChat</h1>
+    </div>
+  );
+}
+
+export default App;
