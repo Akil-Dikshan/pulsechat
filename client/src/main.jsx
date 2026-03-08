4fd610808a7fa564fa481bd910d20a85283ceeca
Akil-Dikshan <akildikshan02@gmail.com>
2026-03-08 18:58:52 +0530

    feat(PC-4): configure WSo2 Asgardeo authentication with AuthProvider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { AuthProvider } from '@asgardeo/auth-react'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
+import { asgardeoConfig } from './config/asgardeo'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  ///<React.StrictMode> which means every single component inside your app now has access to authentication
+  <React.StrictMode>
+    <AuthProvider config={asgardeoConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )
