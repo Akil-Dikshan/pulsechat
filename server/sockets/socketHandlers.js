93abf9b6f22313c163f29d4417bb09f05b11855b
Akil-Dikshan <akildikshan02@gmail.com>
2026-03-25 23:02:14 +0530

    feat(PC-33): implement private_message socket event with db save and emit

--- server/sockets/socketHandlers.js
+++ server/sockets/socketHandlers.js
@@ -1,3 +1,6 @@
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
 const userSocketMap = {};
 
 export const getSocketId = (asgardeoId) => {
@@ -13,9 +16,46 @@ const initSocketHandlers = (io) => {
     userSocketMap[sub] = socket.id;
     console.log("Online users:", Object.keys(userSocketMap).length);
 
+    // private_message event
+    socket.on("private_message", async ({ recipientId, content }) => {
+      try {
+        const sender = await User.findOne({ asgardeoId: sub });
+
+        if (!sender) {
+          return socket.emit("error", { message: "Sender not found" });
+        }
+
+        const recipient = await User.findOne({ asgardeoId: recipientId });
+
+        if (!recipient) {
+          return socket.emit("error", { message: "Recipient not found" });
+        }
+
+        const message = await Message.create({
+          sender: sender._id,
+          recipient: recipient._id,
+          content,
+          type: "text",
+        });
+
+        const populatedMessage = await Message.findById(message._id)
+          .populate("sender", "username avatar")
+          .populate("recipient", "username avatar");
+
+        const recipientSocketId = getSocketId(recipientId);
+        if (recipientSocketId) {
+          io.to(recipientSocketId).emit("private_message", populatedMessage);
+        }
+
+        socket.emit("message_sent", populatedMessage);
+      } catch (error) {
+        console.error("Error in private_message:", error);
+        socket.emit("error", { message: "Failed to send message" });
+      }
+    });
+
     socket.on("disconnect", () => {
       console.log(`User disconnected: ${username || sub} (socket: ${socket.id})`);
-
       delete userSocketMap[sub];
       console.log("Online users:", Object.keys(userSocketMap).length);
     });
