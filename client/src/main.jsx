0697189e39cd48aa9bc32cedb7c42b9aed397bcd
Akil-Dikshan <akildikshan02@gmail.com>
2026-03-28 23:41:22 +0530

    feat(PC-36): create socketcontext to manage socket connection with auth token

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,14 @@ import { AuthProvider } from "@asgardeo/auth-react";
 import App from "./App";
 import "./index.css";
 import { asgardeoConfig } from "./config/asgardeo";
+import { SocketProvider } from "./context/SocketContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  ///<React.StrictMode> which means every single component inside your app now has access to authentication
   <React.StrictMode>
     <AuthProvider config={asgardeoConfig}>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
